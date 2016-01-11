0b4e0e6bcc2f9f087c821d2bf5b5ab5f59f03b76
Ankur Shukla <ankur.shukla03@gmail.com>
2016-01-12 03:09:44 +0530

    Map Added

--- app/src/main/java/lnmiit/madclub/plinth/NavigationDrawerFragment/Map.java
+++ app/src/main/java/lnmiit/madclub/plinth/NavigationDrawerFragment/Map.java
@@ -1,63 +1,37 @@
 package lnmiit.madclub.plinth.NavigationDrawerFragment;
 
-import android.app.Activity;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
+import android.webkit.WebView;
 
 import lnmiit.madclub.plinth.R;
 
-public class Map extends Fragment implements OnMapReadyCallback {
+
+/**
+ * Created by Infinia on 29-11-2015.
+ */
+public class Map extends Fragment {
+
+    WebView map;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
-        initView(v);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_map,null);
+        map = (WebView)v. findViewById(R.id.map);
+        map.getSettings().setBuiltInZoomControls(true);
+        map.getSettings().setDisplayZoomControls(false);
+        map.getSettings().setUseWideViewPort(false);
+        map.scrollTo(1260,700);
+        map.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        map.setScrollbarFadingEnabled(false);
+        map.loadUrl("file:///android_asset/map.html");
         return v;
+
+
     }
-
-
-
-    private void initView(View v)
-    {
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        try {
-            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(26.9363424,75.9213505), 17.0f));
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
-
 }
