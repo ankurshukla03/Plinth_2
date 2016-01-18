ad23103fcee8d02b757a8661e766e168390f9f77
Ankur Shukla <ankur.shukla03@gmail.com>
2016-01-19 02:42:42 +0530

    Schedules link added

--- app/src/main/java/lnmiit/madclub/plinth/NavigationDrawerFragment/Schedule.java
+++ app/src/main/java/lnmiit/madclub/plinth/NavigationDrawerFragment/Schedule.java
@@ -1,5 +1,6 @@
 package lnmiit.madclub.plinth.NavigationDrawerFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,9 +12,13 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+import lnmiit.madclub.plinth.Activity.ListSchedules;
+import lnmiit.madclub.plinth.Activity.TabActivity;
 import lnmiit.madclub.plinth.Adapter.EventsAdapter;
 import lnmiit.madclub.plinth.Adapter.ScheduleAdapter;
+import lnmiit.madclub.plinth.Model.ModelEvents;
 import lnmiit.madclub.plinth.R;
+import lnmiit.madclub.plinth.Widget.RecyclerItemClickListener;
 
 /**
  * Created by Ankur Shukla on 1/11/2016.
@@ -45,6 +50,20 @@ public class Schedule extends Fragment {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new ScheduleAdapter(day,getActivity());
         mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addOnItemTouchListener(new
+                RecyclerItemClickListener(getActivity(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                String temp = day.get(position);
+                Intent i = new Intent(getActivity(), ListSchedules.class);
+                i.putExtra("type", temp);
+                startActivity(i);
+                getActivity().finish();
+
+            }
+        }
+        ));
 
         return view;
     }
